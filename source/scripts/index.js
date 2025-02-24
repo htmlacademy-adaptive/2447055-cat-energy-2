2c620b365cd7b0deea9627901fd11c151a7cd3bc
Im-Risa <ultra.scire@gmail.com>
2025-02-24 17:39:19 +0500

    Вносит финальные правки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,11 +1,13 @@
 /* в этот файл добавляет скрипты*/
 const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-header__toggle');
+const mapFrame = document.querySelector('.contacts__frame');
 
 navMain.classList.remove('main-nav--nojs');
 navMain.classList.add('main-nav--close');
 navToggle.classList.remove('main-header__toggle--nojs');
 navToggle.classList.add('main-header__toggle--close');
+mapFrame.classList.remove('contacts__frame--nojs');
 
 navToggle.addEventListener('click', () => {
   if (navMain.classList.contains('main-nav--close')) {
